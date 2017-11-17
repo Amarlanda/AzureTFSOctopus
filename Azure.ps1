5aba86a7987069d92ed4451868055c96881a31de
Landa <Amar.Landa@ft.com>
2017-11-17 17:35:15 +0000

    bla

---
+++ Azure.ps1
@@ -0,0 +1,13 @@
+﻿import-module azurerm 
+
+Login-AzureRmAccount 
+
+Get-AzureRmSubscription | select name
+
+New-AzureRmResourceGroup -Name "automation-ps-rg" -Location westeurope
+New-AzureRmAutomationAccount -name 'automation-ps-aa' -ResourceGroupName 'automation-ps-rg' -Location westeurope
+
+$rg = 'automation-ps-rg'
+$aa = "automation-ps-aa"
+
+New-AzureRmAutomationRunbook -name 'first-runbook-ps' -Description 'our first runbook' -Type PowerShell -ResourceGroupName $rg -AutomationAccountName $aa
